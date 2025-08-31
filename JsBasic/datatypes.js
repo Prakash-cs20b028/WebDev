212c7374410e479b7aab954f32738b9d039f7c17
Prakash-cs20b028 <Programmer52525@gmail.com>
2025-08-31 18:16:27 +0530

    datatypes are added
    Signed-off-by: Prakash-cs20b028 <Programmer52525@gmail.com>

---
+++ JsBasic/datatypes.js
@@ -0,0 +1,29 @@
+
+console.log(10, typeof 10);
+console.log(3.14, typeof 3.14);
+console.log(NaN, typeof NaN);
+console.log("Prakash",typeof "Prakash")
+console.log('a',typeof 'a');
+console.log(undefined, typeof undefined);
+console.log(true, typeof true);
+console.log(null, typeof null);
+
+
+function func(a,b){
+    return a+b;
+}
+func(1,2);
+
+console.log(func(1,2), typeof func(1,2));
+console.log(func, typeof func);
+
+var a = 10;
+console.log(a, typeof a);
+a = "Prakash";
+console.log(a, typeof a);
+
+let b = function(){
+    return 10*2;
+}
+console.log(b(), typeof b());
+console.log(b, typeof b);
